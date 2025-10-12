5ac6b1d39c65e8a8cba9fbb3ba92fb56200c2670
Jalil David Salamé Messina <jalil.salame@gmail.com>
2025-10-12 21:44:35 +0200

    fix: allow publishing to crates.io
    Needs to be specified as a registry.
    
    Signed-off-by: Jalil David Salamé Messina <jalil.salame@gmail.com>

--- flake-modules/default.nix
+++ flake-modules/default.nix
@@ -41,26 +41,27 @@ in
         name = "release-script";
         runtimeInputs = [ pkgs.release-plz ];
         text = ''
+          declare -ra release_args=(--registry=git-salame-cl --forge=gitea)
           case "$FORGEJO_REF" in
             # On main create a release
             refs/heads/main)
               # Generate a release (won't do anything if the current version is already published)
               echo "Creating release"
-              release-plz release --forge=gitea
+              release-plz release "''${release_args[@]}"
 
               # Create a release PR (will bump the version)
               echo "Creating release PR"
-              release-plz release-pr --forge=gitea
+              release-plz release-pr "''${release_args[@]}"
               ;;
             # Not on main, do a dry-run
             *)
               # Update package version and changelog
               echo "Updating package version and changelog"
-              release-plz update --forge=gitea
+              release-plz update "''${release_args[@]}"
 
               # Do a dry-run
               echo "Release dry-run"
-              release-plz release --forge=gitea --dry-run --allow-dirty
+              release-plz release "''${release_args[@]}" --dry-run --allow-dirty
               ;;
           esac
         '';
